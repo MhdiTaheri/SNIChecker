0ae603a86813abdcc21f6d93303513f38c021696
Mahdi Taheri <151569179+MhdiTaheri@users.noreply.github.com>
2023-12-15 20:22:44 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 clear
 
-echo "Updating and upgrading the server..."
-sudo apt update && sudo apt upgrade -y
-
 echo "Cloning your repository..."
 git clone https://github.com/MhdiTaheri/SNIChecker.git
+
+clear
 
 cd SNIChecker
 
 pip install -r requirements.txt
+
+clear
 
 echo "Running the Python script..."
 python3 sni_checker.py
